4f871dad9856dc27c9782ca31afeb733b62da2d2
nabeelCpp <nabeel_arshad93@outlook.com>
2023-09-25 16:57:14 +0500

    Add Breeze authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, FrontController};
-use App\Http\Controllers\dashboard\{DashboardController, BrandController, ProductController};
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,52 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [FrontController::class, 'index'])->name("home");
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/product/{productId}', [FrontController::class, 'product'])->name("product.details");
-
-/**
- * Brands per page.
- */
-Route::get('/{brand}', [FrontController::class, 'brand'])->name("brand");
-
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    /**
-     * Brands Routes
-     */
-    Route::prefix('brands')->group(function() {
-        Route::get('', [BrandController::class, 'index'])->name('brands');
-        Route::post('', [BrandController::class, 'store'])->name('brands.store');
-        Route::patch('/{id}', [BrandController::class, 'update'])->name('brands.update');
-        Route::delete('/{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
-    });
-
-
-    /**
-     * Products Routes
-     */
-    Route::prefix('products')->group(function() {
-        Route::get('', [ProductController::class, 'index'])->name('products');
-        Route::get('/create', [ProductController::class, 'create'])->name('products.create');
-        Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
-        Route::post('', [ProductController::class, 'store'])->name('products.store');
-        Route::patch('/{id}', [ProductController::class, 'update'])->name('products.update');
-        Route::delete('/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-    });
-
-    /**
-     * Images Routes
-     */
-
-    Route::delete('/images/{id}', [ProductController::class, 'removeImage'])->name('images.remove');
 });
-
 
 require __DIR__.'/auth.php';
